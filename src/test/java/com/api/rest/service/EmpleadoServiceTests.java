3c077a5389ed98f8a0da9a989b925a632cd32db8
Jhon Osorio <jhone.vargaso@uqvirtual.edu.co>
2022-12-23 12:52:09 -0500

    Segundo commit con hora 12:51

--- src/test/java/com/api/rest/service/EmpleadoServiceTests.java
+++ src/test/java/com/api/rest/service/EmpleadoServiceTests.java
@@ -1,13 +1,25 @@
 package com.api.rest.service;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import com.api.rest.model.Empleado;
 import com.api.rest.repository.EmpleadoRepository;
+import com.api.rest.service.Imple.EmpleadoServiceImple;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class EmpleadoServiceTests {
@@ -16,17 +28,52 @@ public class EmpleadoServiceTests {
     private EmpleadoRepository empleadoRepository;
 
     @InjectMocks
-    private EmpleadoService empleadoService;
+    private EmpleadoServiceImple empleadoService;
+
+    private Empleado empleado;
+
+    @BeforeEach
+    void setup(){
+        empleado = Empleado.builder()
+                .nombre("JhonE")
+                .apellido("Osorio")
+                .email("jhon@gmail.com")
+                .build();
+    }
 
     @DisplayName("Test para guardar un empleado")
     @Test
     void testGuardarEmpleado(){
 
         //given
+            given(empleadoRepository.findByEmail(empleado.getEmail()))
+                    .willReturn(Optional.empty());
+            given(empleadoRepository.save(empleado)).willReturn(empleado);
 
         //when
-        //then
+        Empleado empleadoGuardado = empleadoService.saveEmpleado(empleado);
 
+        //then
+        assertThat(empleadoGuardado).isNotNull();
     }
+
+    @DisplayName("Test para guardar un empleado con ThrowException")
+    @Test
+    void testGuardarEmpleadoConThrowException(){
+
+        //given
+        given(empleadoRepository.findByEmail(empleado.getEmail()))
+                .willReturn(Optional.of(empleado));
+        given(empleadoRepository.save(empleado)).willReturn(empleado);
+
+        //when
+        assertThrows(ResourceAccessException.class,() ->{
+            empleadoService.saveEmpleado(empleado);
+        })
+
+        //then
+        verify(empleadoRepository,never()).save(any(Empleado.class));
+    }
+
 
 }
