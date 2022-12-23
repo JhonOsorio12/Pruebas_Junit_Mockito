7f5efe0e3c396a0ca0697cd778354316bfc30e0a
Jhon Osorio <jhone.vargaso@uqvirtual.edu.co>
2022-12-23 14:02:48 -0500

    Tercer commit pruebas Test con Mockito a la capa de Servicios

--- src/test/java/com/api/rest/service/EmpleadoServiceTests.java
+++ src/test/java/com/api/rest/service/EmpleadoServiceTests.java
@@ -4,9 +4,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
 
+import com.api.rest.exception.ResourceNotFoundException;
 import com.api.rest.model.Empleado;
 import com.api.rest.repository.EmpleadoRepository;
 import com.api.rest.service.Imple.EmpleadoServiceImple;
@@ -19,6 +20,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.ResourceAccessException;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,6 +38,7 @@ public class EmpleadoServiceTests {
     @BeforeEach
     void setup(){
         empleado = Empleado.builder()
+                .id(1L)
                 .nombre("JhonE")
                 .apellido("Osorio")
                 .email("jhon@gmail.com")
@@ -64,16 +68,99 @@ public class EmpleadoServiceTests {
         //given
         given(empleadoRepository.findByEmail(empleado.getEmail()))
                 .willReturn(Optional.of(empleado));
-        given(empleadoRepository.save(empleado)).willReturn(empleado);
 
         //when
-        assertThrows(ResourceAccessException.class,() ->{
+        assertThrows(ResourceNotFoundException.class,() ->{
             empleadoService.saveEmpleado(empleado);
-        })
+        });
 
         //then
         verify(empleadoRepository,never()).save(any(Empleado.class));
     }
 
+
+    @DisplayName("Test para listar empleados")
+    @Test
+    void testListarEmpleados(){
+
+        //given
+        Empleado empleado1 = Empleado.builder()
+                .id(2L)
+                .nombre("JhonE")
+                .apellido("Osorio")
+                .email("jhon@gmail.com")
+                .build();
+        given(empleadoRepository.findAll()).willReturn(List.of(empleado,empleado1));
+
+        //when
+        List<Empleado> empleados = empleadoService.getAllEmpleados();
+
+        //then
+        assertThat(empleados).isNotNull();
+        assertThat(empleados.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Test para listar una lista vacia")
+    @Test
+    void testListarColeccionEmpleadosVacia(){
+        //given
+        Empleado empleado1 = Empleado.builder()
+                .id(2L)
+                .nombre("JhonE")
+                .apellido("Osorio")
+                .email("jhon@gmail.com")
+                .build();
+        given(empleadoRepository.findAll()).willReturn(Collections.emptyList());
+
+        //when
+        List<Empleado> empleados = empleadoService.getAllEmpleados();
+
+        //then
+        assertThat(empleados).isEmpty();
+        assertThat(empleados.size()).isEqualTo(0);
+    }
+
+    @DisplayName("Test para obtener empleado por ID")
+    @Test
+    void testObtenerEmpleadoPorID(){
+        //given
+        given(empleadoRepository.findById(1L)).willReturn(Optional.of(empleado));
+
+        //when
+        Empleado empleadoGuardado = empleadoService.getEmpleadoById(empleado.getId()).get();
+
+        //then
+        assertThat(empleadoGuardado).isNotNull();
+    }
+
+    @DisplayName("Test para actualizar empleado")
+    @Test
+    void testActualizarEmpleado(){
+        //given
+        given(empleadoRepository.save(empleado)).willReturn(empleado);
+        empleado.setEmail("Osorio@gmail.com");
+        empleado.setNombre("Juan J");
+
+        //when
+        Empleado empleadoActualizado = empleadoService.updateEmpleado(empleado);
+
+        //then
+        assertThat(empleadoActualizado.getEmail()).isEqualTo("Osorio@gmail.com");
+        assertThat(empleadoActualizado.getNombre()).isEqualTo("Juan J");
+    }
+
+    @DisplayName("Test para eliminar un empleado")
+    @Test
+    void testEliminarEmpleado(){
+        //given
+        long empleadoId = 1L;
+        willDoNothing().given(empleadoRepository).deleteById(empleadoId);
+
+        //when
+        empleadoService.deleteEmpleado(empleadoId);
+
+        //then
+        verify(empleadoRepository,times(1)).deleteById(empleadoId);
+    }
 
 }
