63d052f043ae5a241683268f8c37d6db40f0c351
Jhon Osorio <jhone.vargaso@uqvirtual.edu.co>
2022-12-23 22:08:35 -0500

    Commit Test completo de Empleado Controller Test

--- src/test/java/com/api/rest/controller/EmpleadoControllerTests.java
+++ src/test/java/com/api/rest/controller/EmpleadoControllerTests.java
@@ -18,6 +18,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @WebMvcTest
 public class EmpleadoControllerTests {
 
@@ -57,12 +61,136 @@ public class EmpleadoControllerTests {
         }
 
         @Test
-        void testListarEmpleados(){
+        void testListarEmpleados() throws Exception {
                 //given
+                List<Empleado> listaEmpleados = new ArrayList<>();
+                listaEmpleados.add(Empleado.builder().nombre("Christian").apellido("Ramirez").email("c1@gmail.com").build());
+                listaEmpleados.add(Empleado.builder().nombre("Gabriel").apellido("Ramirez").email("g1@gmail.com").build());
+                listaEmpleados.add(Empleado.builder().nombre("Julen").apellido("Ramirez").email("cj@gmail.com").build());
+                listaEmpleados.add(Empleado.builder().nombre("Biaggio").apellido("Ramirez").email("b1@gmail.com").build());
+                listaEmpleados.add(Empleado.builder().nombre("Adrian").apellido("Ramirez").email("a@gmail.com").build());
+                given(empleadoService.getAllEmpleados()).willReturn(listaEmpleados);
+
                 //when
+                ResultActions response = mockMvc.perform(get("/api/empleados"));
+
                 //then
+                response.andExpect(status().isOk())
+                        .andDo(print())
+                        .andExpect(jsonPath("$.size()",is(listaEmpleados.size())));
         }
 
+        @Test
+        void testObtenerEmpleadoPorID() throws Exception{
+                //given
+                long empleadoId = 1L;
+                Empleado empleado = Empleado.builder()
+                        .nombre("JhonE")
+                        .apellido("Vargas")
+                        .email("jhon@gmail.com")
+                        .build();
+                given(empleadoService.getEmpleadoById(empleadoId)).willReturn(Optional.of(empleado));
 
+                //when
+                ResultActions response = mockMvc.perform(get("/api/empleados/{id}",empleadoId));
 
+                //then
+                response.andExpect(status().isOk())
+                        .andDo(print())
+                        .andExpect(jsonPath("$.nombre",is(empleado.getNombre())))
+                        .andExpect(jsonPath("$.apellido",is(empleado.getApellido())))
+                        .andExpect(jsonPath("$.email",is(empleado.getEmail())));
+        }
+
+        @Test
+        void testObtenerEmpleadoNoEncontrado() throws Exception{
+                //given
+                long empleadoId = 1L;
+                Empleado empleado = Empleado.builder()
+                        .nombre("JhonE")
+                        .apellido("Vargas")
+                        .email("jhon@gmail.com")
+                        .build();
+                given(empleadoService.getEmpleadoById(empleadoId)).willReturn(Optional.empty());
+
+                //when
+                ResultActions response = mockMvc.perform(get("/api/empleados/{id}",empleadoId));
+
+                //then
+                response.andExpect(status().isNotFound())
+                        .andDo(print());
+        }
+
+        @Test
+        void testActualizarEmpleado() throws Exception{
+                //given
+                long empleadoId = 1L;
+                Empleado empleadoGuardado = Empleado.builder()
+                        .nombre("JhonE")
+                        .apellido("Vargas")
+                        .email("jhon@gmail.com")
+                        .build();
+
+                Empleado empleadoActualizado = Empleado.builder()
+                        .nombre("Edison")
+                        .apellido("Osorio")
+                        .email("edison@gmail.com")
+                        .build();
+                given(empleadoService.getEmpleadoById(empleadoId)).willReturn(Optional.of(empleadoGuardado));
+                given(empleadoService.updateEmpleado(any(Empleado.class)))
+                        .willAnswer((invocation)-> invocation.getArgument(0));
+
+                //when
+                ResultActions response = mockMvc.perform(put("/api/empleados/{id}",empleadoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(empleadoActualizado)));
+
+                //then
+                response.andExpect(status().isOk())
+                        .andDo(print())
+                        .andExpect(jsonPath("$.nombre",is(empleadoActualizado.getNombre())))
+                        .andExpect(jsonPath("$.apellido",is(empleadoActualizado.getApellido())))
+                        .andExpect(jsonPath("$.email",is(empleadoActualizado.getEmail())));
+        }
+
+        @Test
+        void testActualizarEmpleadoNoEncontrado() throws Exception{
+                //given - "dado"
+                long empleadoId = 1L;
+                Empleado empleadoGuardado = Empleado.builder()
+                        .nombre("JhonE")
+                        .apellido("Vargas")
+                        .email("jhon@gmail.com")
+                        .build();
+
+                Empleado empleadoActualizado = Empleado.builder()
+                        .nombre("Edison")
+                        .apellido("Osorio")
+                        .email("edison@gmail.com")
+                        .build();
+                given(empleadoService.getEmpleadoById(empleadoId)).willReturn(Optional.empty());
+                given(empleadoService.updateEmpleado(any(Empleado.class)))
+                        .willAnswer((invocation)-> invocation.getArgument(0));
+
+                //when - "cuando"
+                ResultActions response = mockMvc.perform(put("/api/empleados/{id}",empleadoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(empleadoActualizado)));
+
+                //then - "entonces"
+                response.andExpect(status().isNotFound())
+                        .andDo(print());
+        }
+
+        @Test
+        void testEliminarEmpleado() throws Exception{
+                //given
+                long empleadoId = 1L;
+                willDoNothing().given(empleadoService).deleteEmpleado(empleadoId);
+                //when
+                ResultActions response = mockMvc.perform(delete("/api/empleados/{id}",empleadoId));
+                //then
+                response.andExpect(status().isOk())
+                        .andDo(print());
+        }
 }
